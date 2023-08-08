59759a048685c1f533455d67ce8ed9d1d79361d8
Nero Zawady <nerozawady@gmail.com>
2023-08-08 13:11:29 -0800

    Implemented PVC Mode

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ const ESelectPvc = document.querySelector('.select-mode-pvc');
 const EGameScreen = document.querySelector('.game-screen');
 const EScorecardOneName = document.querySelector('.scorecard:nth-child(1) .scorecard-name');
 const EScorecardTwoName = document.querySelector('.scorecard:nth-child(2) .scorecard-name');
-const EScorecardSOnecore = document.querySelector('.scorecard:nth-child(1) .scorecard-score');
+const EScorecardOneScore = document.querySelector('.scorecard:nth-child(1) .scorecard-score');
 const EScorecardTwoScore = document.querySelector('.scorecard:nth-child(2) .scorecard-score');
-const EGameBoard = document.querySelector('.game-board');
 const EBoardCells = document.querySelectorAll('.board-cell');
-const ERoundResult = document.querySelector('.round-result-actual');
+const EGameResult = document.querySelector('.game-result');
+const EGameButtonReplay = document.querySelector('.game-result-replay');
+const EGameButtonRestart = document.querySelector('.game-result-restart');
+
+const WIN_SCORE = 2;
 
 const gameState = {
   mode: '',
@@ -24,19 +27,19 @@ const gameState = {
   board: ['', '', '', '', '', '', '', '', ''],
   turn: 'playerOne',
   turnsPlayed: 0,
-  roundWinner: '',
+  gameWinner: '',
 };
 
-function isWinCond(winCond) {
+function isWinCoord(winCoord) {
   return (
-    gameState.board[winCond[0]] !== '' &&
-    gameState.board[winCond[0]] === gameState.board[winCond[1]] &&
-    gameState.board[winCond[1]] === gameState.board[winCond[2]]
+    gameState.board[winCoord[0]] !== '' &&
+    gameState.board[winCoord[0]] === gameState.board[winCoord[1]] &&
+    gameState.board[winCoord[1]] === gameState.board[winCoord[2]]
   );
 }
 
 function getTurnResult() {
-  const winConds = [
+  const winCoords = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -47,13 +50,31 @@ function getTurnResult() {
     [2, 4, 6],
   ];
 
-  const winCondFinal = winConds.find(isWinCond);
+  const winCoordFinal = winCoords.find(isWinCoord);
 
-  if (winCondFinal !== undefined) {
-    return winCondFinal;
+  if (winCoordFinal !== undefined) {
+    return winCoordFinal;
   }
 
   return null;
+}
+
+function playCell(cell, cellNumber) {
+  if (gameState.board[cellNumber] === '') {
+    if (gameState.turn === 'playerOne') {
+      gameState.board[cellNumber] = 'X';
+      gameState.turn = 'playerTwo';
+      cell.textContent = 'X';
+    } else if (gameState.turn === 'playerTwo') {
+      gameState.board[cellNumber] = 'O';
+      gameState.turn = 'playerOne';
+      cell.textContent = 'O';
+    }
+  }
+
+  gameState.turnsPlayed += 1;
+  cell.classList.remove('empty');
+  cell.disabled = true;
 }
 
 ESelectPvc.addEventListener('click', () => {
@@ -64,59 +85,210 @@ ESelectPvc.addEventListener('click', () => {
   EScorecardOneName.textContent = gameState.playerOne.name;
   EScorecardTwoName.textContent = gameState.playerTwo.name;
   EGameScreen.classList.remove('hidden');
-  // ERoundResult.style.top = `${ERoundResult.offsetHeight}px`;
-  // ERoundResult.style.top = `0px`;
+  document.documentElement.style.setProperty(
+    '--game-result-height',
+    `${EGameResult.offsetHeight}px`
+  );
 });
 
 ESelectPvp.addEventListener('click', () => {
   ESelectModeScreen.classList.add('hidden');
   gameState.mode = 'pvp';
   EGameScreen.classList.remove('hidden');
+  gameState.playerOne.name = 'Player One';
+  gameState.playerTwo.name = 'Player Two';
+  EScorecardOneName.textContent = gameState.playerOne.name;
+  EScorecardTwoName.textContent = gameState.playerTwo.name;
+  document.documentElement.style.setProperty(
+    '--game-result-height',
+    `${EGameResult.offsetHeight}px`
+  );
 });
 
 // TODO - a lot of shared code in the if blocks, take them out
 EBoardCells.forEach(cell =>
   cell.addEventListener('click', () => {
     const cellNumber = cell.getAttribute('data-number');
-    if (gameState.board[cellNumber] === '') {
-      if (gameState.turn === 'playerOne') {
-        gameState.board[cellNumber] = 'X';
-        gameState.turn = 'playerTwo';
-        cell.textContent = 'X';
-      } else if (gameState.turn === 'playerTwo') {
-        gameState.board[cellNumber] = 'O';
-        gameState.turn = 'playerOne';
-        cell.textContent = 'O';
+    playCell(cell, cellNumber);
+
+    if (gameState.turnsPlayed >= 5) {
+      const winCoord = getTurnResult();
+      if (winCoord !== null) {
+        if (gameState.board[winCoord[0]] === 'X') {
+          gameState.playerOne.score += 1;
+          EScorecardOneScore.textContent = gameState.playerOne.score;
+          gameState.gameWinner = gameState.playerOne.name;
+        } else {
+          gameState.playerTwo.score += 1;
+          EScorecardTwoScore.textContent = gameState.playerTwo.score;
+          gameState.gameWinner = gameState.playerTwo.name;
+        }
+
+        EBoardCells.forEach(cell => {
+          cell.disabled = true;
+        });
+
+        EBoardCells[winCoord[0]].classList.add('win-cell');
+        EBoardCells[winCoord[1]].classList.add('win-cell');
+        EBoardCells[winCoord[2]].classList.add('win-cell');
+
+        if (gameState.playerOne.score < WIN_SCORE && gameState.playerTwo.score < WIN_SCORE) {
+          setTimeout(() => {
+            resetRound();
+          }, 1500);
+        } else {
+          EGameResult.children[0].textContent = `${gameState.gameWinner} won the game`;
+          EGameResult.classList.add('game-result-show');
+          // setTimeout(() => {
+          //   EGameResult.classList.add('game-result-hide');
+          //   EGameResult.classList.remove('game-result-show');
+          //   roundEnd();
+          // }, 2000);
+          // setTimeout(() => {
+          //   EGameResult.classList.remove('game-result-hide');
+          // }, 2500);
+        }
+      } else if (winCoord === null && gameState.board.find(cell => cell === '') === undefined) {
+        setTimeout(() => {
+          resetRound();
+        }, 1500);
       }
     }
 
-    gameState.turnsPlayed += 1;
-    cell.classList.remove('empty');
-    cell.disabled = true;
-
+    playComputerRound();
     if (gameState.turnsPlayed >= 5) {
-      const winCond = getTurnResult();
-      if (winCond !== null) {
-        if (gameState.board[winCond[0]] === 'X') {
+      const winCoord = getTurnResult();
+      if (winCoord !== null) {
+        if (gameState.board[winCoord[0]] === 'X') {
           gameState.playerOne.score += 1;
-          gameState.roundWinner = gameState.playerOne.name;
+          EScorecardOneScore.textContent = gameState.playerOne.score;
+          gameState.gameWinner = gameState.playerOne.name;
         } else {
           gameState.playerTwo.score += 1;
-          gameState.roundWinner = gameState.playerTwo.name;
+          EScorecardTwoScore.textContent = gameState.playerTwo.score;
+          gameState.gameWinner = gameState.playerTwo.name;
         }
+
+        EBoardCells.forEach(cell => {
+          cell.disabled = true;
+        });
+
+        EBoardCells[winCoord[0]].classList.add('win-cell');
+        EBoardCells[winCoord[1]].classList.add('win-cell');
+        EBoardCells[winCoord[2]].classList.add('win-cell');
+
+        if (gameState.playerOne.score < WIN_SCORE && gameState.playerTwo.score < WIN_SCORE) {
+          setTimeout(() => {
+            resetRound();
+          }, 1500);
+        } else {
+          EGameResult.children[0].textContent = `${gameState.gameWinner} won the game`;
+          EGameResult.classList.add('game-result-show');
+          // setTimeout(() => {
+          //   EGameResult.classList.add('game-result-hide');
+          //   EGameResult.classList.remove('game-result-show');
+          //   roundEnd();
+          // }, 2000);
+          // setTimeout(() => {
+          //   EGameResult.classList.remove('game-result-hide');
+          // }, 2500);
+        }
+      } else if (winCoord === null && gameState.board.find(cell => cell === '') === undefined) {
+        setTimeout(() => {
+          resetRound();
+        }, 1500);
       }
-
-      EBoardCells[winCond[0]].classList.add('win-cell');
-      EBoardCells[winCond[1]].classList.add('win-cell');
-      EBoardCells[winCond[2]].classList.add('win-cell');
-
-      ERoundResult.textContent = `${gameState.roundWinner} won the round`;
-      ERoundResult.classList.add('show');
-      setTimeout(() => {
-        ERoundResult.classList.add('hide');
-      }, 2500);
     }
   })
 );
 
-// TODO - reset data after round ends.
+EGameButtonReplay.addEventListener('click', () => {
+  resetReplay();
+  EGameResult.classList.remove('game-result-show');
+  EGameResult.classList.add('game-result-hide');
+  setTimeout(() => {
+    EGameResult.classList.remove('game-result-hide');
+  }, 500);
+});
+
+EGameButtonRestart.addEventListener('click', () => {
+  resetGame();
+  EGameResult.classList.remove('game-result-show');
+  EGameResult.classList.remove('game-result-hide');
+  EGameScreen.classList.add('hidden');
+  ESelectModeScreen.classList.remove('hidden');
+});
+
+// TODO - tie
+
+function resetRound() {
+  gameState.board = ['', '', '', '', '', '', '', '', ''];
+  gameState.turn = 'playerOne';
+  gameState.turnsPlayed = 0;
+  gameState.gameWinner = '';
+
+  EBoardCells.forEach(cell => {
+    cell.textContent = '';
+    cell.disabled = false;
+    cell.classList.add('empty');
+    cell.classList.remove('win-cell');
+  });
+}
+
+function resetReplay() {
+  gameState.board = ['', '', '', '', '', '', '', '', ''];
+  gameState.turn = 'playerOne';
+  gameState.turnsPlayed = 0;
+  gameState.gameWinner = '';
+
+  EBoardCells.forEach(cell => {
+    cell.textContent = '';
+    cell.disabled = false;
+    cell.classList.add('empty');
+    cell.classList.remove('win-cell');
+  });
+
+  gameState.playerOne.score = 0;
+  gameState.playerTwo.score = 0;
+  EScorecardOneScore.textContent = '0';
+  EScorecardTwoScore.textContent = '0';
+}
+
+function resetGame() {
+  gameState.mode = '';
+  gameState.playerOne.name = '';
+  gameState.playerOne.score = 0;
+  gameState.playerTwo.name = '';
+  gameState.playerTwo.score = 0;
+  gameState.board = ['', '', '', '', '', '', '', '', ''];
+  gameState.turn = 'playerOne';
+  gameState.turnsPlayed = 0;
+  gameState.gameWinner = '';
+
+  EBoardCells.forEach(cell => {
+    cell.textContent = '';
+    cell.disabled = false;
+    cell.classList.add('empty');
+    cell.classList.remove('win-cell');
+  });
+
+  EScorecardOneName.textContent = '';
+  EScorecardOneScore.textContent = '0';
+  EScorecardTwoName.textContent = '';
+  EScorecardTwoScore.textContent = '0';
+}
+
+function playComputerRound() {
+  const emptyCells = [];
+  for (let i = 0; i < gameState.board.length; i++) {
+    const cell = gameState.board[i];
+    if (cell === '') {
+      emptyCells.push(i);
+    }
+  }
+
+  const chosenCell = Math.floor(Math.random() * emptyCells.length);
+  playCell(EBoardCells[chosenCell], emptyCells[chosenCell]);
+}
+
+// TODO - computer RNG play
